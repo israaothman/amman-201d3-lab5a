2401212961cf526eafb3fcdca72b478e875aec5d
israaothman <israaayoub@hotmail.com>
2020-02-20 13:30:13 +0200

    solving prob2

--- app.js
+++ app.js
@@ -15,16 +15,20 @@ function sum(a, b) { //eslint-disable-line
   var sum= a+b;
   return [sum ,'The sum of 4 and 7 is 11.'] ;
 }
-testSum();
+
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiply() that takes in two numbers
+ as arguments and returns an array where
+  the first element is the product of those numbers,
+   and the second element is a string that EXACTLY follows this example 
+   and uses the values that were input into the function:
 
 "The product of 5 and 9 is 45."
 
@@ -32,11 +36,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+   var mul = a*b;
+   return [mul,'The product of 5 and 9 is 45.'];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
